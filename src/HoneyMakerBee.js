20b8303e503153ba76c452fbe01b930396a61605
Harry Clemente <hclemente1989@gmail.com>
2020-10-12 12:07:59 -0700

    Passed all Spec tests

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Grub.call(this);
+  this.age = 10;
+  this.job = "make honey";
+  this.color = "yellow";
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
